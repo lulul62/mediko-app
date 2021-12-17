b2ccdf642a93bc895b599d6b92d95aa58af7c723
lulul62 <leplan.ludwig@gmail.com>
2021-12-17 10:40:07 +0100

    fix: remove useless import

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -4,20 +4,16 @@ import { LoginRoutingModule } from './login-routing.module';
 
 import { LoginComponent } from './login.component';
 
-import { NzButtonModule } from 'ng-zorro-antd/button';
-
-import { NzCardModule } from 'ng-zorro-antd/card';
-import { FormsModule } from '@angular/forms';
-
 
 
 @NgModule({
-  imports: [LoginRoutingModule, NzButtonModule, NzCardModule, FormsModule],
+  imports: [LoginRoutingModule],
   declarations: [LoginComponent],
   exports: [LoginComponent],
   providers: [
   ],
 })
+
 export class LoginModule {
 
 }
